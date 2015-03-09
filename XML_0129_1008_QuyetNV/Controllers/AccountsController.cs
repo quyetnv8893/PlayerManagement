992ec1ec94c91bd8868f25c2a4156bc2ee0cb14f
Phạm Quốc Thắng <thangpq@outlook.com.vn>
2015-03-09 23:56:15 +0700

    Added OWIN. Not use yet

--- XML_0129_1008_QuyetNV/Controllers/AccountsController.cs
+++ XML_0129_1008_QuyetNV/Controllers/AccountsController.cs
@@ -8,6 +8,8 @@ using System.Web;
 using System.Web.Mvc;
 using PlayerManagement.Models;
 using System.Web.Security;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security;
 
 namespace PlayerManagement.Controllers
 {
@@ -24,11 +26,26 @@ namespace PlayerManagement.Controllers
         [HttpPost]
         public ActionResult Login([Bind(Include = "Username,MD5Password")] Account account)
         {
+            bool isPersistent = true;
             if (ModelState.IsValid)
             {
                 if (isValid(account.Username, account.MD5Password))
-                {                   
+                {
+                    string userData = "ApplicationSpecific data for this user.";
 
+                    FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(1,
+                      account.Username,
+                      DateTime.Now,
+                      DateTime.Now.AddMinutes(30),
+                      isPersistent,
+                      userData,
+                      FormsAuthentication.FormsCookiePath);
+
+                    // Encrypt the ticket.
+                    string encTicket = FormsAuthentication.Encrypt(ticket);
+
+                    // Create the cookie.                  
+                    Response.Cookies.Add(new HttpCookie(FormsAuthentication.FormsCookieName, encTicket));
                     FormsAuthentication.SetAuthCookie(account.Username, false);
                     return RedirectToAction("Index", "Players");
                 }
@@ -40,6 +57,16 @@ namespace PlayerManagement.Controllers
             return View(account);
         }
 
+        private async Task SignInAsync(Account account, bool isPersistent)
+        {            
+            
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Players");
+        }
         private bool isValid(string username, string password)
         {
             bool isValid = false;
